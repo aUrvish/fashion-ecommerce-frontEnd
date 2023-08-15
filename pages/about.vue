dd3aa520a6cb40df6b5318ec5b3c805d641b51b7
Urvish <mandaliyaurvishj7@gmail.com>
2023-08-15 21:42:17 +0530

    modified about pages

--- pages/about.vue
+++ pages/about.vue
@@ -5,64 +5,60 @@
             <TopBanner head="About Us" />
 
             <div class="dd-containt">
-                <h1>About pages</h1>
-                <p>
-                    Welcome to Desi Dukaan, your ultimate destination for a seamless online shopping experience. As
-                    passionate
-                    curators of quality and style, we're here to redefine the way you shop in the digital realm.
-                    <br> <br>
-                    At Desi Dukaan, we're not just an online store – we're a gateway to an expansive world of products that
-                    cater to
-                    your every need and desire. From trendsetting fashion pieces to cutting-edge gadgets, from home
-                    essentials
-                    that
-                    transform spaces to unique finds that spark joy – we've carefully curated a collection that reflects
-                    your
-                    diverse tastes.
-                    <br> <br>
-                    Our commitment goes beyond providing products; it's about crafting a journey. With a user-friendly
-                    interface,
-                    secure transactions, and prompt customer support, we're dedicated to making your shopping experience as
-                    delightful as it is convenient.
-                    <br> <br>
-                    Join us in embracing the future of shopping. Explore, discover, and indulge in the art of e-commerce
-                    with
-                    Desi
-                    Dukaan. Your desires, just a click away."
-                    <br> <br>
-                    Remember to replace "Desi Dukaan" with your actual brand name, and adjust the other details to match
-                    your
-                    e-commerce platform's unique offerings and approach.
-                    <br> <br>
-                    Here at Desi Dukaan, we're passionate about bringing you a curated selection of items
-                    that cater to your every interest and need. From the latest tech gadgets that redefine innovation, to
-                    fashion trends that make a statement, and home essentials that transform spaces into sanctuaries – we've
-                    assembled it all for you.
-                    <br> <br>
-                    Our virtual shelves are open 24/7, ensuring that your shopping cravings are met whenever they strike.
-                    With
-                    secure transactions, lightning-fast delivery, and a user-friendly interface, we're dedicated to making
-                    your
-                    online shopping experience as seamless as possible.
-                    <br> <br>
-                    Join us in embracing the future of commerce. Explore the endless aisles of Desi Dukaan,
-                    where your next discovery is just a click away."
-                    <br> <br>
-                    Feel free to substitute "Desi Dukaan" with your actual platform's name and adjust other
-                    details according to your platform's unique features and offerings.
-                </p>
+                <div class="dd-about-img">
+                    <img src="../assets/images/about.png" alt="AboutUs" style="width: 100%;">
+                </div>
+                <div class="dd-about-text">
+                    <h1>About pages</h1>
+                    <p>
+                        Welcome to Desi Dukaan, your ultimate destination for a seamless online shopping experience. As
+                        passionate
+                        curators of quality and style, we're here to redefine the way you shop in the digital realm.
+                        <br> <br>
+                        At Desi Dukaan, we're not just an online store – we're a gateway to an expansive world of products
+                        that
+                        cater to
+                        your every need and desire. From trendsetting fashion pieces to cutting-edge gadgets, from home
+                        essentials
+                        that
+                        transform spaces to unique finds that spark joy – we've carefully curated a collection that reflects
+                        your
+                        diverse tastes.
+                        <br> <br>
+                        Our commitment goes beyond providing products; it's about crafting a journey. With a user-friendly
+                        interface,
+                        secure transactions, and prompt customer support, we're dedicated to making your shopping experience
+                        as
+                        delightful as it is convenient.
+                    </p>
+                </div>
             </div>
         </div>
     </NuxtLayout>
 </template>
 
-<style scoped>.dd-about {
+<style scoped lang="scss">
+.dd-about {
     max-width: 1536px;
     margin: 60px auto 0;
     font-family: 'Nunito';
-    
+
     .dd-containt {
         margin: 0 auto;
         width: 90%;
+        display: flex;
+        align-items: center;
+
+        .dd-about-img , .dd-about-text {
+            width: 100%;
+            padding: 10px;
+        }
+    }
+
+
+    @media screen and (max-width : 991px) {
+        .dd-containt {
+            flex-direction: column;
+        }
     }
 }</style>
